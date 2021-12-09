769a27d26c0707fc4e95d9ffa1b4fec932dc9357
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-12-09 22:33:24 +0100

    fix: initial commit

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.core.model.temporal.Temporal;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.DataStoreException;
+import com.amplifyframework.datastore.generated.model.Priority;
+import com.amplifyframework.datastore.generated.model.Todo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            //Amplify.addPlugin(new AWSApiPlugin()); // UNCOMMENT this line once backend is deployed
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("Amplify", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("Amplify", "Could not initialize Amplify", error);
+        }
+
+        Todo item = Todo.builder()
+                .name("Novi task")
+                .description("Opis")
+                .priority(Priority.LOW)
+                .completedAt(new Temporal.DateTime("1970-01-01T12:30:23.999Z"))
+                .build();
+        Amplify.DataStore.save(
+                item,
+                success -> Log.i("Amplify", "Saved item: " + success.item().getId()),
+                error -> Log.e("Amplify", "Could not save item to DataStore", error)
+        );
+    }
+}
